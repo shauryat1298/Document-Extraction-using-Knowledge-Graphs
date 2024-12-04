b54aa94a649902aca3179c73e313c06c7c731b14
Shaurya Tripathi <shauryat1298@gmail.com>
2024-12-03 21:12:01 -0600

    progress

--- src/utils/call_gpt4o.py
+++ src/utils/call_gpt4o.py
@@ -10,6 +10,8 @@ def call_openai_endpoint(img_path: str):
   system_prompt = """You are an experienced seasoned underwriter working in Senior Living Insurance Facility space. 
   You know all the important information that you need from an application to write the best coverage policy.
   You understand all types of policies like General Liability, Professional Liability, commercial property insurance etc.
+
+  
   """
 
   
